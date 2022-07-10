f560c39e55bd82cc5907fe29b4c80f39b799b283
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-10 19:12:47 +0900

    1330번 문제

---
+++ src/main/java/baekjoon/controlflow/Cf1330.java
@@ -0,0 +1,19 @@
+package baekjoon.controlflow;
+
+import java.util.Scanner;
+
+public class Cf1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        }
+        else if (a == b) {
+            System.out.println("==");
+        }
+    }
+}
